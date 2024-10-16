5f18f109448a387af54dd7ee7c82eda701662833
Mandar Todkar <todkarmandar897@gmail.com>
2024-10-17 00:25:43 +0530

    Naw add signup code

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Signup from './Signup';
+
+function App() {
+  return (
+    <>
+    <Signup/>
+   
+  </>
+  );
+}
+
+export default App;
---
+++ my-app/src/Signup.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const Signup = () => {
+    const handleInput=() =>{
+        
+    }
+  return (
+    
+      <div classNameS='container'>
+        <div className='form-container'>
+          <div className='form'>
+            <h2>Signup</h2>
+            <input type='text'placeholder='Name' />
+            <input type='email'placeholder='Email'/>
+            <input type='password'placeholder='Password' />
+            <a href='#'>Forgot Password</a>
+            <button>Signup</button>
+            </div>
+
+          
+        </div>
+        
+        </div>
+        
+    
+    
+
+  );
+}
+
+export default Signup;
